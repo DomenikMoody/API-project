40a651aaf1e6883c2cd86885dbdb2cfad58cfdf4
Domenik Moody <Domeniktmoody@gmail.com>
2023-03-15 16:27:06 -0400

    keep

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -19,3 +19,10 @@ router.get("/api/csrf/restore", (req, res) => {
   // ...
 
 module.exports = router;
+
+// backend/routes/index.js
+// ...
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+// ...
