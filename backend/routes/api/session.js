f84e1eb325ea5392a0cecae44d505456a23165ff
Domenik Moody <Domeniktmoody@gmail.com>
2023-03-16 15:23:54 -0400

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -15,7 +15,7 @@ router.post(
     '/',
     async (req, res, next) => {
       const { credential, password } = req.body;
-  
+
       const user = await User.unscoped().findOne({
         where: {
           [Op.or]: {
@@ -24,7 +24,7 @@ router.post(
           }
         }
       });
-  
+
       if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
         const err = new Error('Login failed');
         err.status = 401;
@@ -32,19 +32,32 @@ router.post(
         err.errors = { credential: 'The provided credentials were invalid.' };
         return next(err);
       }
-  
+
       const safeUser = {
         id: user.id,
         email: user.email,
         username: user.username,
       };
-  
+
       await setTokenCookie(res, safeUser);
-  
+
       return res.json({
         user: safeUser
       });
     }
   );
+  // backend/routes/api/session.js
+// ...
+
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
+  // ...
 
 module.exports = router;
