2acef2d3f5a36b72c2685fc3fc6fe59f7ba1b07b
Domenik Moody <Domeniktmoody@gmail.com>
2023-03-21 00:12:04 -0400

    those young eyes

--- backend/db/seeders/20230320215502-Bookings.js
+++ backend/db/seeders/20230320215502-Bookings.js
@@ -12,20 +12,20 @@ module.exports = {
       {
         spotId:1,
         userId:4,
-        startDate:2023-12-11,
-        endDate: 2024-0o1-12
+        startDate:"2023-12-11",
+        endDate: "2024-01-12"
       },
       {
         spotId:2,
         userId:3,
-        startDate:2023-12-11,
-        endDate: 2024-0o1-12
+        startDate:"2023-12-11",
+        endDate: "2024-01-12"
       },
       {
         spotId:3,
         userId:1,
-        startDate:2023-12-11,
-        endDate: 2024-0o1-12
+        startDate:"2023-12-11",
+        endDate: "2024-01-12"
       },
 
     ],{} );
