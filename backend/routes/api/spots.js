65036ac05540ac8af68ad08173574d33c94c5c65
Domenik Moody <Domeniktmoody@gmail.com>
2023-03-21 19:21:53 -0400

    GASOBTCU

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { Spot } = require('../../db/models');
 const { Review } = require('../../db/models');
 const { SpotImage } = require('../../db/models');
+const { User } = require('../../db/models');
 
 
 const router = express.Router();
@@ -45,6 +46,56 @@ router.get('/', async (req, res) => {
 
     })
     return res.json({ "Spots": spotList })
+})
+
+
+router.get('/current', async (req,res)=>{
+    const {user} = req
+    if (user){
+        const ownedSpot = await Spot.findAll({
+            where: {
+                ownerId: user.id
+            },
+            include: [
+                {
+                    model: Review
+                },
+                {
+                    model: SpotImage
+                }
+            ]
+        })
+        let spotList = []
+        ownedSpot.forEach(spot => {
+            spotList.push(spot.toJSON())
+        })
+        spotList.forEach(element => {
+            let num = element.Reviews.length
+            let sum = 0;
+            element.Reviews.forEach(star => {
+                sum += star.stars
+            })
+            if (num === 0) {
+                element.avgRating = 0
+            }else {
+                element.avgRating = (sum / num).toFixed(1)
+            }
+            let url = ""
+            element.SpotImages.forEach(spot => {
+                url += spot.url
+            })
+            element.previewImage = url
+
+        })
+        spotList.forEach(obj => {
+            delete obj['Reviews'];
+            delete obj['SpotImages'];
+
+        })
+        return res.json({ "Spots": spotList })
+    } else return res.json({user: null})
+
+
 })
 
 module.exports = router;
