083e5f37f1c836b992d594b671b36c013cc7b639
Domenik Moody <Domeniktmoody@gmail.com>
2023-03-21 00:08:14 -0400

    fixed to thing

--- backend/db/migrations/20230318233840-create-review.js
+++ backend/db/migrations/20230318233840-create-review.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 let options = {};
 if (process.env.NODE_ENV === 'production'){
-  options.schema = process.env.schema;
+  options.schema = process.env.SCHEMA;
 }
 module.exports = {
   async up(queryInterface, Sequelize) {
