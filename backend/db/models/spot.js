a0c61b30f7432582a86558e787d118aa129de84f
Domenik Moody <Domeniktmoody@gmail.com>
2023-04-16 18:14:47 -0400

    text fix

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Spot.belongsTo(models.User, {foreignKey:'ownerId', onDelete: "cascade", hooks: "true"});
+      Spot.belongsTo(models.User, {foreignKey:'ownerId'});
       Spot.hasMany(models.Booking, {foreignKey: 'spotId', onDelete: "cascade", hooks: "true"} )
       Spot.hasMany(models.SpotImage, {foreignKey: 'spotId', onDelete: "cascade", hooks: "true"});
       Spot.hasMany(models.Review, {foreignKey: 'spotId', onDelete: "cascade", hooks: "true"})
